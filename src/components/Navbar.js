1019120bcc828f859e9c401a9f644b01a965350f
Dulyarat455 <Dulyarat.455@gmail.com>
2023-07-24 23:20:25 +0700

    1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,31 @@
 import Link from 'next/link';
+import { useEffect,useState } from 'react';
+import { useRouter } from 'next/router';
+import jwt from "jsonwebtoken";
+
 
 const Navbar = () => {
+  const [token, setToken] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+    // if(!token){
+    //   router.push("/login");
+    // }
+  }, []);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    // window.location.reload();
+    router.push("/login");
+    
+      
+   
+  };
+
+
   return (
     <nav className="bg-green-500 p-4">
       <ul className="flex items-center">
@@ -9,7 +34,9 @@ const Navbar = () => {
             <p className="text-white font-semibold">Home</p>
           </Link>
         </li>
-        <li className="mr-6">
+       { !token && (
+       <>
+       <li className="mr-6">
           <Link href="/login">
             <p className="text-white font-semibold">Login</p>
           </Link>
@@ -19,6 +46,10 @@ const Navbar = () => {
             <p className="text-white font-semibold">register</p>
           </Link>
         </li>
+        </>)
+      }
+      { token && (
+      <>
         <li className="mr-6">
           <Link href="/lists">
             <p className="text-white font-semibold">lists</p>
@@ -29,6 +60,13 @@ const Navbar = () => {
             <p className="text-white font-semibold">account</p>
           </Link>
         </li>
+        <li className="mr-6">
+          <Link href="" onClick={logout}>
+            <p className="text-white font-semibold">Logout</p>
+          </Link>
+        </li>
+        </> )   
+        }
         {/* Add more navigation links here */}
       </ul>
     </nav>
